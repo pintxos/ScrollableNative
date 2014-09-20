551108d8da180f4072011f69c9742c4e11211b1a
Jelle Versele <jelle@versele.be>
2014-09-20 18:53:30 +0200

    WIP

--- index.js
+++ index.js
@@ -13,12 +13,41 @@
 
 	define([], function () {
 
+		var ScrollableNative, _defaults, _props;
+
+		/* Defaults
+		----------------------------------------------- */
+		_defaults = {
+			orientation: 'horizontal',
+			selectors: {
+				scrollableEl: '> .main > .wrap'
+			}
+		};
+
+		_props = {
+			horizontal: {
+				scrollPos: 'scrollLeft',
+				scrollSize: 'scrollWidth',
+				size: 'width',
+				offset: 'left'
+			},
+
+			vertical: {
+				scrollPos: 'scrollTop',
+				scrollSize: 'scrollHeight',
+				size: 'height',
+				offset: 'top'
+			}
+		};
 
 		/* Constructor
 		----------------------------------------------- */
-		var ScrollableNative = function () {
-
+		var ScrollableNative = function (el, options) {
+			this._settings = $.extend(true, {}, _default, options);
+			Component.call(el, this._settings);
 		};
+
+		inherit(ScrollableNative, Component);
 
 
 		/* Methods
@@ -29,16 +58,47 @@
 		 * @return {void}
 		 */
 		ScrollableNative.prototype.init = function () {
+			Component._super.init.call(this);
+		};
 
+		ScrollableNative.prototype._getProp = function (prop) {
+			return _props[this._settings.orientation][prop];
+		};
+
+		ScrollableNative.prototype.getScrollPos = function () {
+			return Math.floor(this.getScrollableEl()[this._getProp('scrollPos')]());
+		};
+
+		ScrollableNative.prototype.setScrollPos = function (pos) {
+			this.getScrollableEl()[this._getProp('scrollPos')](pos);
+		};
+
+		ScrollableNative.prototype.getMaskSize = function () {
+			return Math.ceil(this.getScrollableEl()[this._getProp('size')]());
+		};
+
+		ScrollableNative.prototype.getMaxScrollPos = function () {
+			return this.getScrollableEl()[0][this._getProp('scrollSize')] - this.getScrollableEl()[this._getProp('size')]();
+		};
+
+		ScrollableNative.prototype.isEndReached = function (offset) {
+			offset = (typeof offset === 'undefined') ? 0 : offset;
+			return this.getScrollPos() >= (this.getMaxScrollPos() - offset);
+		};
+
+		ScrollableNative.prototype.isBeginReached = function (offset) {
+			offset = (typeof offset === 'undefined') ? 0 : offset;
+			return this.getScrollPos() <= offset;
 		};
 
 		/**
-		 * All teardown logic should go here
-		 * @return {void}
+		 * Getter for scrollable el
+		 * @return {jQuery}
 		 */
-		ScrollableNative.prototype.destroy = function () {
-
+		ScrollableNative.prototype.getScrollableEl = function () {
+			return this._query(this.getSettings().selectors.scrollableEl);
 		};
+
 
 
 		/* Event handlers
