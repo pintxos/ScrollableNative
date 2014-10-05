e8c40d3be9ca1a06fff33c20e13d8f021960ffa4
Jelle Versele <jelle.versele@internetarchitects.be>
2014-10-05 21:09:34 +0200

    Fixed grunt setup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,12 @@
-module.exports = function (grunt) {
-
-	require('load-grunt-tasks')(grunt);
-
-	grunt.initConfig({
-
-		karma: {
-			options: {
-				basePath: '',
-				files: [
-					'index.js',
-					'test/*.js'
-				],
-				frameworks: [
-					'jasmine'
-				]
-			},
-			dev: {
-				browsers: ['Chrome']
-			},
-			ci: {
-				browsers: ['PhantomJS'],
-				singleRun: true
-			}
-		},
-
-		jshint: {
-			files: ['*.js'],
-			options: {
-				strict: true,
-				es3: true,
-				globals: {
-					window: true,
-					document: true,
-					define: true
-				}
-			}
-		}
-	});
-
-	grunt.registerTask('default', []);
-	grunt.registerTask('test', ['jshint', 'karma:dev']);
-	grunt.registerTask('testCI', ['jshint', 'karma:ci']);
-
+var config = {
+	testDependencies: [
+		'bower_components/jquery/dist/jquery.js',
+		'bower_components/pintxos-inherit/index.js',
+		'bower_components/pintxos-destroyable/index.js',
+		'bower_components/pintxos-component/index.js',
+		'tests/*.js',
+		'index.js'
+	]
 };
+
+module.exports = require('grunt-pintxos')(config);
