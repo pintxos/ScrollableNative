285ddf9091a2c7ab33c917758ffbaafa2fd6ec27
Jelle Versele <jelle.versele@internetarchitects.be>
2014-12-30 10:34:00 +0100

    WIP

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 var config = {
 	testDependencies: [
-		'tests/fixtures/**/*.html',
+		'tests/fixtures/**/*',
 		'bower_components/jquery/dist/jquery.js',
 		'bower_components/jasmine-jquery/lib/jasmine-jquery.js',
 		'bower_components/pintxos-inherit/index.js',
--- tests/index.js
+++ tests/index.js
@@ -1,15 +1,134 @@
-beforeEach(function () {
-	jasmine.getFixtures().fixturesPath = '/base/tests/fixtures/';
-	loadFixtures('index.html');
+'use strict';
 
-	$slider = $('#container');
-	slider = new pintxos.ScrollableNative($slider).init();
+var scrollable, $div;
+
+jasmine.getFixtures().fixturesPath = '/base/tests/fixtures/';
+jasmine.getStyleFixtures().fixturesPath = '/base/tests/fixtures/';
+
+function setUpScrollable(options) {
+	$div = $('.container');
+	scrollable = new pintxos.ScrollableNative($div[0], options);
+	scrollable.init();
+}
+
+beforeEach(function () {
+
+	loadStyleFixtures('style.css');
+});
+
+describe('general', function () {
+
+	sharedTests('horizontal');
+	sharedTests('vertical');
+
+
 });
 
 
-describe('setup', function () {
+function sharedTests (orientation) {
 
-	it("should work", function () {
+	loadFixtures(orientation + '.html');
+
+
+
+	it('should work', function () {
 		expect(true).toBe(true);
+	})
+}
+
+
+
+
+describe('horizontal scrolling', function () {
+
+	beforeEach(function () {
+
+		loadFixtures('horizontal.html');
+
+		setUpScrollable({
+			orientation: 'horizontal'
+		});
+	});
+
+	afterEach(function () {
+		scrollable.destroy();
+	});
+
+	it("should set scrollLeft", function () {
+
+		var pos;
+
+		pos = 10;
+
+		scrollable.setScrollPos(pos);
+
+		expect($div.scrollLeft()).toBe(pos);
+		expect(scrollable.getScrollPos()).toBe(pos);
+	});
+
+	it("should get the maskwidth correct", function () {
+
+		expect(scrollable.getMaskSize()).toBe($div.width());
+	});
+
+	it("should return the right maximum scroll position", function () {
+
+		var maxScrollPos;
+
+		maxScrollPos = $div[0].scrollWidth - $div.width();
+		expect(maxScrollPos).toBe(scrollable.getMaxScrollPos());
+	});
+
+
+
+	it('should returns the right properties', function () {
+		expect(scrollable._getProp('scrollPos')).toBe('scrollLeft');
+		expect(scrollable._getProp('scrollSize')).toBe('scrollWidth');
+		expect(scrollable._getProp('size')).toBe('width');
+		expect(scrollable._getProp('offset')).toBe('left');
 	});
 });
+
+describe('vertical scrolling', function () {
+
+	beforeEach(function () {
+
+		loadFixtures('vertical.html');
+
+		setUpScrollable({
+			orientation: 'vertical'
+		});
+	});
+
+	it("should set scrollTop", function () {
+
+		var pos;
+
+		pos = 10;
+
+		$div.scrollTop(10);
+		expect($div.scrollTop()).toBe(pos);
+	});
+
+	it("should get the mask height correct", function () {
+
+		expect(scrollable.getMaskSize()).toBe($div.height());
+	});
+
+	it("should return the right maximum scroll position", function () {
+
+		var maxScrollPos;
+
+		maxScrollPos = $div[0].scrollHeight - $div.height();
+		expect(maxScrollPos).toBe(scrollable.getMaxScrollPos());
+	});
+
+	it('should returns the right properties', function () {
+		expect(scrollable._getProp('scrollPos')).toBe('scrollTop');
+		expect(scrollable._getProp('scrollSize')).toBe('scrollHeight');
+		expect(scrollable._getProp('size')).toBe('height');
+		expect(scrollable._getProp('offset')).toBe('top');
+	});
+
+});
+
